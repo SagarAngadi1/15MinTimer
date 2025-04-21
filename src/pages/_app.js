f8d574292146ac309df5a9a327846ac4e95a728f
Sagar Angadi <ianassagar@gmail.com>
2025-04-21 11:31:16 +0530

    Initial commit Launch Version

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,6 @@
-import "@/styles/globals.css";
+//import "@/styles/globals.css";
+import '../styles/globals.css'; // if _app.js is in src/pages/
+
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
