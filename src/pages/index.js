bc1b61e7f91ed364199f0f99d7c3081725c28b01
Sagar Angadi <ianassagar@gmail.com>
2025-04-21 17:07:16 +0530

    Initial new commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -161,7 +161,7 @@ export default function Home() {
           animate={{ scale: 1 }}
           transition={{ delay: 1, duration: 1 }}
           // className="relative w-[900px] h-[900px] flex items-center justify-center"
-          className="relative w-[90vw] sm:w-[500px] md:w-[900px] aspect-square flex items-center justify-center mt-10 sm:mt-10 md:mt-0"
+          className="relative w-[90vw] sm:w-[500px] md:w-[900px] aspect-square flex items-center justify-center mt-14 sm:mt-14 md:mt-0"
         >
           <svg
             width="700"
