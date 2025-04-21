d9e4aeb9c7552ab6fb0fab0962ac4d78f47c4c03
Sagar Angadi <ianassagar@gmail.com>
2025-04-21 20:57:26 +0530

    Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import { motion } from "framer-motion";
 import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from 'next/router';
+import fetchCurrentUser from '../../utils/fetchCurrentUser';
+
 
 
 export default function Home({ currentUser }) {
